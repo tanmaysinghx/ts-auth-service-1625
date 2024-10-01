a39a5845fc4c530cea153ab5150361a0509b837c
Tanmay Singh <tanmaysinghx@gmail.com>
2024-10-01 22:35:44 +0530

    Added SSL certicifcates

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,30 @@ import app from './app';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './config/db';
 import logger from './utils/logger';
+import fs from 'fs';
+import https from 'https';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 1625;
+
+/// Ensure that PFX_PATH and PFX_PASSPHRASE are available
+const pfxPath = process.env.PFX_PATH || 'src/certificates/mykeystore.p12';
+const passphrase = process.env.PFX_PASSPHRASE;
+
+if (!pfxPath || !passphrase) {
+  throw new Error("PFX_PATH and PFX_PASSPHRASE environment variables must be defined");
+}
+
+const options = {
+  pfx: fs.readFileSync(pfxPath),
+  passphrase: passphrase
+};
+
+// Create HTTPS server
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS server running on port 443 with p12 certificate');
+});
 
 app.listen(PORT, async () => {
   logger.info(`Server is running on port: ${PORT}`);
