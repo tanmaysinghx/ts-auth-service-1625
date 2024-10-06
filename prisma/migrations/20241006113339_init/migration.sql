d4083d1fc1a61b4cc657d5b0e9fee503c4164312
Tanmay Singh <tanmaysinghx@gmail.com>
2024-10-06 17:07:16 +0530

    deployment

--- prisma/migrations/20241006113339_init/migration.sql
+++ prisma/migrations/20241006113339_init/migration.sql
@@ -24,13 +24,24 @@ CREATE TABLE `Role` (
 
 -- CreateTable
 CREATE TABLE `Audit` (
-    `id` VARCHAR(8) NOT NULL,
-    `userId` VARCHAR(8) NOT NULL,
-    `roleId` VARCHAR(4) NOT NULL,
+    `id` VARCHAR(255) NOT NULL,
+    `userId` VARCHAR(36) NOT NULL,
+    `roleId` VARCHAR(36) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `operation` VARCHAR(255) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userIpAddress` VARCHAR(45) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Otp` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `otp` VARCHAR(191) NOT NULL,
+    `expiry` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
